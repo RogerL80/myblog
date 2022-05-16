28217b7c5b5fccd87a9535a183c4fd4a9c0a2fe3
Roger Lozada <rogerlcolombia@gmail.com>
2022-05-16 16:20:13 -0500

    Modificacion inicial Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            My First Blog
         </h2>
-    </x-slot>
+    </x-slot> --}}
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis corporis hic, sed dolore qui molestiae ut cum. Explicabo dignissimos, illo beatae, illum officia commodi iste tenetur error laudantium, libero nobis!</p>
+                <p>Magni quas aspernatur minus error quod ad vel suscipit aliquid nihil, saepe ipsum, cum, necessitatibus nostrum odit facere earum iusto voluptatibus mollitia nulla nemo odio commodi corrupti. Fugiat, impedit blanditiis.</p>
+                <p>Tenetur, eos velit perspiciatis sapiente ab in ad doloribus cumque, voluptatum officiis quasi accusantium nemo aliquid suscipit temporibus aperiam ea adipisci corporis et nobis maiores mollitia ut quis laudantium? Suscipit.</p>
+                <p>Adipisci accusamus praesentium consequuntur hic numquam sequi dolore iste reprehenderit incidunt assumenda architecto molestiae dignissimos commodi, consectetur officia autem velit pariatur, doloribus quisquam. A quam, totam vel consectetur quae deserunt?</p>
+                <p>Illo magni eos fugit quod modi doloribus vitae nostrum iusto maxime! Quaerat hic odit rerum ipsam commodi ipsa, adipisci est illo sed tempore beatae totam officiis pariatur accusantium eius et.</p>
             </div>
         </div>
     </div>
